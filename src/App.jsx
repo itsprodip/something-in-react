f16ad058d74f44d528cbee849e49f02a1d6ad8c7
nothing36 <prodipsarker953@gmail.com>
2025-11-23 21:57:56 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './Components/Navbar';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar></Navbar>
     </div>
   );
 };
