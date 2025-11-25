b12748c595c4c77c6caba09db160fb6d8bdef0ef
nothing36 <prodipsarker953@gmail.com>
2025-11-25 17:58:07 +0600

    data charts added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import Navbar from "./Components/Navbar";
 import CusNav from "./Components/CusNav";
 import PricingCards from "./Components/PricingCards";
+import ResultCharts from "./Components/ResultCharts";
 
 const App = () => {
   const pricingData= fetch('pricingData.json') .then(res=>res.json());
@@ -12,6 +13,7 @@ const App = () => {
       <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
         <PricingCards pricingData={pricingData}></PricingCards>
       </Suspense>
+      <ResultCharts></ResultCharts>
     </div>
   );
 };
